2cbec0da1ffccbc61fae7973325644d91db1f9ed
rakhadavin <rakha.davin@ui.ac.id>
2023-09-20 15:47:47 +0700

    Tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
 from main.views import show_main, create_product,  show_json, show_xml_by_id, show_json_by_id 
-
+from django.contrib.auth import logout
 app_name = "main"
 urlpatterns = [
     path('',views.show_main,name="show_main"),
     path('create-product',views.create_product,name="create_product"),
-path('json/', show_json, name='show_json'), 
-path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    
+    
+    path('json/', show_json, name='show_json'), 
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
